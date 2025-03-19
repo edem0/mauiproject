7efc62e33971d74eb8e346ee38693488dd44582c
hedi <b1hodhed@vasvari.org>
2025-03-19 09:18:19 +0100

    jury model

---
+++ Solution.Core/Models/JuryModel.cs
@@ -0,0 +1,16 @@
+﻿using Solution.Database.Entities;
+
+namespace Solution.Core.Models;
+
+public class JuryModel
+{
+    public uint Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string EmailAddress { get; set; }
+
+    public string PhoneNumber { get; set; }
+
+    public uint CompetitionId { get; set; }
+}
